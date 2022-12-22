442e9db7d52fbb34ce8ce7de8b5a3ef17d8f0416
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-12-22 21:54:05 +0600

    all data load and specific data load with dynamic router

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.quaequt.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -18,6 +18,20 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const serviceCollection = client.db("rentCar").collection("services");
+
+        app.get("/services",async(req,res)=>{
+                const query = {}
+                const cursor = serviceCollection.find(query);
+                const services  = await cursor.toArray();
+                res.send(services);
+        })
+
+        app.get("/services/:id",async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
     }
     finally{
 
